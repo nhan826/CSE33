c0ffad63f422e8a5ec37e0cbb72a8b8aaaab5681
nah758 <nah758@g.harvard.edu>
2022-02-20 19:06:13 -0500

    final wiki changes

--- wiki/encyclopedia/urls.py
+++ wiki/encyclopedia/urls.py
@@ -1,14 +1,14 @@
-"""docstring"""
+"""urls.py"""
 from django.urls import path
-
 from . import views
 
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("wiki/<str:entry>/edit", views.edit, name = "edit"),
+    path("create", views.create, name = "create"),
+    path("random", views.random_page, name = "random"),
+    path("search", views.search, name = "search" ),
     # url path to display entry contents at /wiki/TITLE
     path("wiki/<str:entry>", views.entry, name="entry"),
-    path("search", views.search, name = "search" ),
-    path("create", views.create, name = "create"),
-    path("random", views.random_page, name = "random")
 ]
--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,4 +1,4 @@
-"""docstring"""
+"""views.py file for wiki encyclopedia"""
 import random
 from markdown2 import markdown
 from django import forms
@@ -13,44 +13,37 @@ class CreateNewForm(forms.Form):
     # references:
     # https://stackoverflow.com/questions/1080650/removing-the-label-from-djangos-textarea-widget
     # http://www.learningaboutelectronics.com/Articles/How-to-create-a-text-area-in-a-Django-form.php
-    new_title = forms.CharField(label = "Entry Title")   
-    new_article = forms.CharField(label = "", help_text = "", widget=forms.Textarea)
-
-class EditForm(forms.Form):
-    """Form for editing a page"""
-    # references:
-    # https://stackoverflow.com/questions/1080650/removing-the-label-from-djangos-textarea-widget
-    # http://www.learningaboutelectronics.com/Articles/How-to-create-a-text-area-in-a-Django-form.php
-    edit_text = forms.CharField(label = "", help_text = "", widget=forms.Textarea)
+    new_title = forms.CharField(label=" Entry Title ")
+    new_article = forms.CharField(label="", help_text="", widget=forms.Textarea)
 
 
-def index(request):    
-    """docstring"""
+
+def index(request):
+    """function renders index.html and provides a list of entries"""
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()   # stores list of entries
     })
 
 def entry(request, entry):
-    """docstring"""
+    """Function for displaying entry"""
     content = util.get_entry(entry) # retrieves encyclopedia entry
     if content is None:   # if no entry then an error page is rendered
         return render(request, "encyclopedia/error.html", {
             "entry": entry
-        })       
+        })
     # if entry exists, then content is displayed
-    return render(request, "encyclopedia/entry.html", {  
+    return render(request, "encyclopedia/entry.html", {
         "content": markdown(content),    # markdown converted to html
-        "entry" : entry.capitalize(),    # capitalized the entry name (used in html title)
+        "entry" : entry,
     })
 
-
 def search(request):
-    """docstring"""
+    """Functionality for Wiki search form"""
     entry = request.GET.get("q")    #saves query
     entry_name = util.get_entry(entry)    #  retrieves entry of query
     if entry_name:     # if entry exists then user is brought to the page for that entry
         return HttpResponseRedirect(reverse("entry",
-            kwargs={"entry" : entry}))
+            kwargs={"entry" : entry}))   # reference: https://stackoverflow.com/questions/26197545/django-cant-pass-parameter-in-httpresponseredirect
     else:     #   if no entry exists with that specific name:
         matches = []   #   a list is made of titles that have the query as a substring
         for title in util.list_entries():
@@ -61,10 +54,10 @@ def search(request):
         })
 
 def create(request):
-    """docstring"""
+    """Functionality for creating a new Wiki article"""
     # converts the list of entries to lowercase
     # reference: https://www.delftstack.com/howto/python/python-lowercase-list/
-    lowercase_list = [title.lower() for title in util.list_entries()]   
+    lowercase_list = [title.lower() for title in util.list_entries()]
 
     if request.method == "POST":
         form = CreateNewForm(request.POST)
@@ -77,13 +70,44 @@ def create(request):
             })
         else:
             util.save_entry(new_title, new_article)    # if its a new entry, the entry is saved
+            return HttpResponseRedirect(reverse("entry",
+            kwargs={"entry" : new_title}))
+
 
     return render(request, "encyclopedia/create.html", {
         "form": CreateNewForm(),    # refers to class CreateNewForm()
+
+    })
+
+def edit(request, entry):
+    """Functionality for editing Wiki entries"""
+    content = util.get_entry(entry)   # retrieves entry to later prepopulate textarea
+
+    # I had trouble changing the "initial" value when this form was outside of the function.
+    # Though not ideal, I was able to change the value by including the class inside the edit function.
+    class EditForm(forms.Form):
+        """Form for editing a page"""
+        # references:
+        # https://stackoverflow.com/questions/1080650/removing-the-label-from-djangos-textarea-widget
+        # http://www.learningaboutelectronics.com/Articles/How-to-create-a-text-area-in-a-Django-form.php
+        edit_text = forms.CharField(initial = content, label="", help_text="", widget=forms.Textarea)
+
+    if request.method == "POST":
+        form = EditForm(request.POST)
+        if form.is_valid():
+            edit_text = form.cleaned_data["edit_text"]    
+            util.save_entry(entry, edit_text)   # saves edited entry
+            return HttpResponseRedirect(reverse("entry",
+            kwargs={"entry" : entry}))
+
+    return render(request, "encyclopedia/edit.html", {
+        "form2": EditForm(),
+        "entry" : entry,
+        "content": content
     })
 
 def random_page(request):
-    """docstring"""
+    """Chooses a random entry and redirects to the entry page"""
     random_entry = random.choice(util.list_entries())
     return HttpResponseRedirect(reverse("entry",
             kwargs = {"entry" : random_entry}))
