0e16e5dc2799603893f9836f2c58d5ac97be0583
nah758 <nah758@g.harvard.edu>
2022-03-08 22:52:07 -0500

    third commerce commit

--- commerce/auctions/models.py
+++ commerce/auctions/models.py
@@ -4,35 +4,42 @@ from django.db import models
 
 class User(AbstractUser):
     """Model to represent users"""
-    watching = models.ManyToManyField("Listing", related_name = "watchers" )
+    watching = models.ManyToManyField("Listing", related_name="watchers")
+
 
 class Category(models.Model):
     """This model represents different categories of items"""
-    category_name = models.CharField(max_length= 64)
+    category_name = models.CharField(max_length=64)
+
     def __str__(self):
         return self.category_name
-    
+
 
 class Listing(models.Model):
     """This model represents the auction listings"""
-    title = models.CharField(max_length= 64)
+    title = models.CharField(max_length=64)
     description = models.TextField()
     current_price = models.DecimalField(max_digits=6, decimal_places=2)  # reference: https://stackoverflow.com/questions/1139393/what-is-the-best-django-model-field-to-use-to-represent-a-us-dollar-amount
     is_active = models.BooleanField(default=True)
     picture = models.URLField(null=True, blank=True)
-    picture_text = models.CharField(blank=True, default = "", max_length= 64)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name = "listings")
-    seller = models.ForeignKey(User,on_delete=models.CASCADE, related_name= "listings")
+    picture_text = models.CharField(blank=True, default="", max_length=64)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="listings")
+    seller = models.ForeignKey(User, on_delete=models.CASCADE, related_name="listings")
+    closed = models.BooleanField(default=False)
+
 
 class Bid(models.Model):
     """This model creates the fields required for bidding"""
-    amount = models.IntegerField()
-    listing = models.ForeignKey(Listing,on_delete=models.CASCADE, related_name = "bids")
-    bidder = models.ForeignKey(User,on_delete=models.CASCADE, related_name = "bids")
+    amount = models.DecimalField(max_digits=6, decimal_places=2)
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids")
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bids")
+
+    def __str__(self):
+        return str(self.amount)
+
 
 class Comment(models.Model):
     """This model creates fields required for commenting on listings"""
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name = "comments")
-    text_comment = models.TextField(max_length= 128)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name = "comments" )
-
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="comments")
+    text_comment = models.TextField(max_length=128)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="comments")
--- commerce/auctions/views.py
+++ commerce/auctions/views.py
@@ -7,24 +7,29 @@ from django.urls import reverse
 from django.forms import ModelForm
 
 
-from .models import User, Listing, Category, Comment
+from .models import Bid, User, Listing, Category, Comment
 
 
 class CreateNewListing(ModelForm):
     class Meta:
         model = Listing
-        fields = ["title", "description", "current_price", "picture", "picture_text", "category" ]
-    
+        fields = ["title", "description", "current_price", "picture", "picture_text", "category"]
+
+
 class CreateNewComment(ModelForm):
     class Meta:
         model = Comment
         fields = ["text_comment"]
-    
 
+
+class CreateBid(ModelForm):
+    class Meta:
+        model = Bid
+        fields = ["amount"]
 
 
 def index(request):
-    """This function renders the index page and provides the listings database """
+    """This function renders the index page and provides the listings database"""
     return render(request, "auctions/index.html", {
         "listings": Listing.objects.all()
     })
@@ -60,7 +65,7 @@ def logout_view(request):
 
 
 def register(request):
-    """This view renders a registration form, and creates a new user 
+    """This view renders a registration form, and creates a new user
     when the form is submitted. """
     if request.method == "POST":
         username = request.POST["username"]
@@ -90,80 +95,112 @@ def register(request):
 
 def create_listing(request):
     """This view renders the create listing form. If the request method
-     is POST the sumbited data is saved to the listing database and the user 
+     is POST the sumbited data is saved to the listing database and the user
      is redirected to the index page."""
     if request.method == "POST":
         form = CreateNewListing(request.POST)
         if form.is_valid():
             listing = form.save(commit=False)
             listing.seller = request.user   # The user is included as the seller
-            listing.save() 
+            listing.save()
             return HttpResponseRedirect(reverse("index"))
 
     form = CreateNewListing()
     return render(request, "auctions/create_listing.html", {
-        "form": form,    
+        "form": form,
     })
 
 
 def listing_page(request, listing_id):
     """This view renders a page for each auction listing"""
-    listing_page = Listing.objects.get(id = listing_id)    # retrieves listing by id
+    # retrieves listing by id
+    listing_page = Listing.objects.get(id=listing_id)
     form = CreateNewComment()
+    bid_form = CreateBid()
 
-    # reference for including multiple forms in a view: https://stackoverflow.com/questions/866272/how-can-i-build-multiple-submit-buttons-django-form
+    # sets the starting bid as the first bid object
+    
+    if listing_page.bids.all():
+        pass
+    else:
+        starter_bid = Bid(amount = listing_page.current_price, listing = listing_page, bidder = request.user )
+        starter_bid.save()
+    last_bids = listing_page.bids.last()
+
+
+    # reference for including multiple forms in a view:
+    # https://stackoverflow.com/questions/866272/how-can-i-build-multiple-submit-buttons-django-form
     if request.method == "POST" and 'create_comment' in request.POST:
         form = CreateNewComment(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
-            comment.author = request.user   
+            # includes addition data to save comment
+            comment.author = request.user      
             comment.listing = listing_page
-            comment.save() 
-            return HttpResponseRedirect(reverse("listing_page", kwargs={"listing_id" : listing_id}))
+            comment.save()
+            return HttpResponseRedirect(reverse("listing_page", kwargs={"listing_id": listing_id}))
 
     if request.method == "POST" and 'add_watchlist' in request.POST:
+        # adds the listing to the user's watchlist
         listing_page.watchers.add(request.user)
-        listing_page.save()   
+        listing_page.save()
 
     if request.method == "POST" and 'remove_watchlist' in request.POST:
+        # removes the listing to the user's watchlist
         current_user = request.user
         current_user.watching.remove(listing_page)
 
-
+    if request.method == "POST" and 'place_bid' in request.POST:
+        form = CreateBid(request.POST)
+        if form.is_valid():
+            new_bid = form.save(commit=False)
+            # includes addition data to save comment
+            new_bid.bidder = request.user      
+            new_bid.listing = listing_page
+            new_bid.save()
+            return HttpResponseRedirect(reverse("listing_page", kwargs={"listing_id": listing_id}))
     
+    if request.method == "POST" and 'close_page' in request.POST:
+        # removes the listing to the user's watchlist
+        listing_page.closed = True
+        listing_page.save()
 
     return render(request, "auctions/listing_page.html", {
-        "current_user" : request.user,
-        "watchers" : listing_page.watchers.all(),
-        "form" : form,
-        "listing": listing_page,   
+        "current_user": request.user,
+        "watchers": listing_page.watchers.all(),
+        "form": form,
+        "bid_form": bid_form,
+        "listing": listing_page,
         "comment": listing_page.comments.all(),
+        "last_bids": last_bids,
+        # the last bid is used to set a minimum value required in the CreateBid Field
+        "lower_bound": round((float(last_bids.amount) + (0.01)), 2)
     })
 
+
 def category_page(request):
+    """This view renders a page that displays links for diffent item categories"""
     categories = Category.objects.all()
     return render(request, "auctions/category_page.html", {
         "categories": categories,
     })
-    
+
+
 def category_contents(request, category_id):
-    category_content = Category.objects.get(id = category_id)    # retrieves listing by id
+    """This view renders the pages for items within a category, and provides
+    the page with all the listings in a given category"""
+    category_content = Category.objects.get(id=category_id)   
     category_listings = category_content.listings.all()
     return render(request, "auctions/category_contents.html", {
         "category_content": category_content,
-        "category_listings": category_listings   
+        "category_listings": category_listings
     })
+
 
 def watchlist(request):
-        current_user = User.objects.get(username = request.user)
-        return render(request, "auctions/watchlist.html", {
+    """This view renders a page where users can see what items are on their
+    watchlist"""
+    current_user = User.objects.get(username=request.user)
+    return render(request, "auctions/watchlist.html", {
         "watchlist": current_user.watching.all()
     })
-
-
-
-
-
-
-
-
